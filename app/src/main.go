c772f4d1f715d6aa680f41d665c96afca1c630a3
Ken Lyon <fatstrat389@gmail.com>
2019-10-05 18:18:35 -0700

    Updated DB initialization to allow specifying a db name

--- app/src/main.go
+++ app/src/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "io/ioutil"
-    "model"
-    "net/http"
-    "strconv"
-    "strings"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"model"
+	"net/http"
+	"strconv"
+	"strings"
 
-    _ "github.com/lib/pq" //driver for postgres
+	_ "github.com/lib/pq" //driver for postgres
 )
 
 func main() {
-    portNumber, url := getParameters()
-    connectToDatabase(url)
-    model.RegisterRoutes()
-    http.ListenAndServeTLS(":"+portNumber, "certs/cert.pem", "certs/key.pem", nil)
+	portNumber, url := getParameters()
+	connectToDatabase(url)
+	model.RegisterRoutes()
+	http.ListenAndServeTLS(":"+portNumber, "certs/cert.pem", "certs/key.pem", nil)
 }
 
 func connectToDatabase(url string) {
-    db, err := sql.Open("postgres", url)
-    if err != nil {
-        fmt.Println(err)
-    }
-    model.SetDatabase(db)
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	model.SetDatabase(db, "priustask")
 }
 
 /**
@@ -32,33 +32,33 @@ func connectToDatabase(url string) {
  * file. Return the port number and the databaseUrl as a string.
  */
 func getParameters() (portNumber, databaseURL string) {
-    file, err := ioutil.ReadFile("params")
+	file, err := ioutil.ReadFile("params")
 
-    //verify the file is not null:
-    if err != nil {
-        panic(err)
-    }
+	//verify the file is not null:
+	if err != nil {
+		panic(err)
+	}
 
-    //Split the file into a slice of lines:
-    mySlice := strings.Split(string(file), "\n")
+	//Split the file into a slice of lines:
+	mySlice := strings.Split(string(file), "\n")
 
-    //Test if the port number specified is a properly formatted number:
-    _, err = strconv.Atoi(mySlice[0][5:])
-    if strings.HasPrefix(mySlice[0], "port=") && err == nil {
-        //port_number = mySlice[1][5:]
-        portNumber = mySlice[0][5:]
-        fmt.Println("Listening on port ", portNumber)
-    } else {
-        panic("Malformed params file. port identifier 'port=' and port number expected.")
-    }
+	//Test if the port number specified is a properly formatted number:
+	_, err = strconv.Atoi(mySlice[0][5:])
+	if strings.HasPrefix(mySlice[0], "port=") && err == nil {
+		//port_number = mySlice[1][5:]
+		portNumber = mySlice[0][5:]
+		fmt.Println("Listening on port ", portNumber)
+	} else {
+		panic("Malformed params file. port identifier 'port=' and port number expected.")
+	}
 
-    //Verify prefix for line 2 and assign databaseURL:
-    if strings.HasPrefix(mySlice[1], "db=") && len(mySlice[1]) > 9 {
-        fmt.Println("database URL: ", mySlice[1])
-        databaseURL = mySlice[1][3:]
-    } else {
-        panic("Malformed params file. database identifier 'db=' and database url expected.")
-    }
+	//Verify prefix for line 2 and assign databaseURL:
+	if strings.HasPrefix(mySlice[1], "db=") && len(mySlice[1]) > 9 {
+		fmt.Println("database URL: ", mySlice[1])
+		databaseURL = mySlice[1][3:]
+	} else {
+		panic("Malformed params file. database identifier 'db=' and database url expected.")
+	}
 
-    return portNumber, databaseURL
+	return portNumber, databaseURL
 }
--- app/src/model/db.go
+++ app/src/model/db.go
@@ -3,8 +3,10 @@ package model
 import "database/sql"
 
 var db *sql.DB
+var dbName string
 
 //SetDatabase for this application
-func SetDatabase(database *sql.DB) {
-    db = database
+func SetDatabase(database *sql.DB, n string) {
+	db = database
+	dbName = n
 }
